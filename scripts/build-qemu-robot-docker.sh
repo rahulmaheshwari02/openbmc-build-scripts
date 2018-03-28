2eda1969e951f7226d600cdcc35a2ad335624cd9
rahulmaheshwari01 <rahulmaheshwari01@gmail.com>
2018-03-28 12:53:19 +0530

    Update build-qemu-robot-docker.sh

--- scripts/build-qemu-robot-docker.sh
+++ scripts/build-qemu-robot-docker.sh
@@ -84,13 +84,13 @@ RUN make install
 
 RUN wget https://github.com/mozilla/geckodriver/releases/download/v0.20.0/geckodriver-v0.20.0-linux64.tar.gz
 RUN tar -xvf geckodriver-*.tar.gz
-RUN cp geckodriver /usr/local/bin
-RUN pwd
-RUN ls
-RUN ls -la /usr/local/bin
-RUN chmod 777 /usr/local/bin/geckodriver
-RUN ls -la /usr/local/bin
-RUN /usr/local/bin/geckodriver &
+#RUN cp geckodriver /usr/local/bin
+#RUN pwd
+#RUN ls
+#RUN ls -la /usr/local/bin
+#RUN chmod 777 /usr/local/bin/geckodriver
+#RUN ls -la /usr/local/bin
+RUN export PATH=$PATH:/geckodriver
 
 RUN grep -q ${GROUPS} /etc/group || groupadd -g ${GROUPS} ${USER}
 RUN grep -q ${UID} /etc/passwd || useradd -d ${HOME} -m -u ${UID} -g ${GROUPS} \
